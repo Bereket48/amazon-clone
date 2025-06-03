4a63513d989b79447697adaa6b584c3549d05e20
Bereket48 <abatebereket536@gmail.com>
2025-06-03 10:57:35 +0200

    Fix import path casing for Layout component

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Layout from "../../components/Layout/Layout";
+// import Layout from "../../components/Layout/Layout"; //Netlify deployment commented this as reason for deployment error
+import Layout from "../../components/LayOut/LayOut"
 import Carousel from "../../components/Carousel/CarouselEffect";
 import Category from "../../components/Category/Category"; //Unless updated he went with CategoryCard here, mistake????????
 import Product from "../../components/Product/Product"
