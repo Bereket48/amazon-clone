df2a687c8880869ad35ed858356527509f4978e4
Bereket48 <abatebereket536@gmail.com>
2025-06-03 11:50:31 +0200

    Fix vite config: add React plugin and base path

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
 })
 
 
-  // base: '/netflix-clone/', // Add this above plugins when ready to deploy. 
+
+
 
 
 
